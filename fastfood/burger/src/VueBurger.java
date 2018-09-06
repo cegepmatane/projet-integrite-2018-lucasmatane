c439ee20e562cf5840031beda5ad8388c46ae0f6
Lucas Sehairi <lucassehairi@MacBook-Pro-de-Lucas-2.local>
2018-09-06 00:48:57 -0400

    ajout de fichiers manquants

---
+++ fastfood/burger/src/VueBurger.java
@@ -0,0 +1,52 @@
+import javafx.application.Application;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+
+public class VueBurger extends Application {
+
+	protected Label valeurNom;
+	protected Label valeurFromage;
+	protected Label valeurPrix;
+	protected Label valeurViande;
+	
+	
+	
+	public void start(Stage stade) throws Exception {
+		
+ 		Pane panneau = new Pane();	
+		GridPane grilleBurger = new GridPane();
+
+		
+		valeurNom = new Label("");
+		grilleBurger.add(new Label("Nom : "), 0, 0);
+		grilleBurger.add(valeurNom, 1, 0);
+		
+		valeurFromage = new Label("");
+		grilleBurger.add(new Label("fromage : "), 0, 1);
+		grilleBurger.add(valeurFromage, 1, 1);
+
+		valeurPrix = new Label("");
+		grilleBurger.add(new Label("Prix : "), 0, 2);
+		grilleBurger.add(valeurPrix, 1, 2);		
+
+		valeurViande = new Label("");
+		grilleBurger.add(new Label("Viande : "), 0, 3);
+		grilleBurger.add(valeurViande, 1, 3);				
+	}
+	
+	public void afficherBurger(Burger burger)
+	{
+		this.valeurNom.setText(burger.getNom());
+		this.valeurFromage.setText(burger.getFromage());
+		this.valeurPrix.setText(burger.getPrix());
+		this.valeurViande.setText(burger.getViande());	
+	}
+
+
+	
+	
+
+}
---
+++ fastfood/burger/src/VueListeBurger.java
@@ -0,0 +1,58 @@
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+public class VueListeBurger extends Application
+{
+	private GridPane grilleBurger = null;
+	
+	@Override
+	public void start(Stage stade) throws Exception {
+		Pane panneau = new Pane();	
+ 		grilleBurger = new GridPane();
+ 		
+ 		
+ 		panneau.getChildren().add(grilleBurger);
+ 		stade.setScene(new Scene(panneau, 400, 400));
+ 		stade.show();		
+ 		
+ 		List listeBurgerTest = new ArrayList<Burger>();
+ 		listeBurgerTest.add(new Burger("Cheesy", "Mozza", "3$12", "Steack"));
+ 		listeBurgerTest.add(new Burger("Backone", "Cheddar", "2$12", "Beacon"));
+ 		listeBurgerTest.add(new Burger("Giant", "Cheddar et Mozza", "3$70", "Steack"));
+ 		listeBurgerTest.add(new Burger("Carnivore", "Cheddar", "3$", "steack + Cheddar"));
+ 		this.afficherListeBurger(listeBurgerTest); 
+		
+	}
+	
+	public void afficherListeBurger(List<Burger> listeBurger)
+	{
+		this.grilleBurger.getChildren().clear();
+		
+		int numero = 0;
+		this.grilleBurger.add(new Label("Nom"), 0, numero);
+		this.grilleBurger.add(new Label("Viande"), 1, numero);	
+		for(Burger burger : listeBurger)
+		{
+			numero++;
+			this.grilleBurger.add(new Label(burger.getNom()), 0, numero);
+			this.grilleBurger.add(new Label(burger.getViande()), 1, numero);			
+			this.grilleBurger.add(new Button("Editer"), 2, numero);
+		}
+	}
+
+
+	
+	
+
+
+}
