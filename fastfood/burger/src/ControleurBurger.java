3cab7213b7dad88a5e28748edf54aff8f0a2b546
Lucas Sehairi <lucassehairi@MacBook-Pro-de-Lucas-2.local>
2018-09-12 17:01:40 -0400

    creation du controleur + liste

---
+++ fastfood/burger/src/ControleurBurger.java
@@ -0,0 +1,11 @@
+public class ControleurBurger {
+	
+	private NavigateurDesVues navigateur;
+	
+	public ControleurBurger(NavigateurDesVues navigateur)
+	{
+		this.navigateur = navigateur;
+		System.out.println("Initialisation du controleur");
+	}
+
+}
